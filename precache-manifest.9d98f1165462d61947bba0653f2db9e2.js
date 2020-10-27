96af51be0d6c918488be4939244f174d8e8ff5f1
changangus <63685366+changangus@users.noreply.github.com>
2020-10-26 21:22:29 -0400

    Updates

---
+++ precache-manifest.9d98f1165462d61947bba0653f2db9e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01380e56d21bee4c7bf8ea4162e92f76",
+    "url": "/community_fridge_finder/index.html"
+  },
+  {
+    "revision": "8d5c31eef6ff97da6c0b",
+    "url": "/community_fridge_finder/static/css/main.d24f2300.chunk.css"
+  },
+  {
+    "revision": "60cce6657c8dbda5474d",
+    "url": "/community_fridge_finder/static/js/2.e6d2f1fd.chunk.js"
+  },
+  {
+    "revision": "397893adeca610c9fda4fe97323ae340",
+    "url": "/community_fridge_finder/static/js/2.e6d2f1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5c31eef6ff97da6c0b",
+    "url": "/community_fridge_finder/static/js/main.101d7ab6.chunk.js"
+  },
+  {
+    "revision": "491abc3ceda7ceeb22d3",
+    "url": "/community_fridge_finder/static/js/runtime-main.cc7b2c9e.js"
+  },
+  {
+    "revision": "93aa59c83aa1e4d35971ad13c57d8a92",
+    "url": "/community_fridge_finder/static/media/diet.93aa59c8.svg"
+  },
+  {
+    "revision": "b6ae0d2c352cda76c8c17b2dcf28e290",
+    "url": "/community_fridge_finder/static/media/like.b6ae0d2c.svg"
+  },
+  {
+    "revision": "673dea17ff3c0683c98c30724af69067",
+    "url": "/community_fridge_finder/static/media/volunteer.673dea17.svg"
+  }
+]);
